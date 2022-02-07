971fc98967ee618e1ddf2b8921be8eee1f70d199
sergioj97 <sergiojsancam@gmail.com>
2022-02-07 16:18:47 +0100

    Añadido formulario y handler para crear zumbos

--- src/main/java/com/example/basiccrud/zumbo/ZumboController.java
+++ src/main/java/com/example/basiccrud/zumbo/ZumboController.java
@@ -1,14 +1,20 @@
 package com.example.basiccrud.zumbo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/zumbos")
 public class ZumboController {
+	
+	@Autowired
+	private ZumboRepository repositorio_zumbos;
 	
 	@GetMapping("/lista")
 	public String mostrarPantallaListaZumbos(Model model) {
@@ -23,6 +29,14 @@ public class ZumboController {
 	@GetMapping("/crear")
 	public String mostrarPantallaCrearZumbo(Model model) {
 		return "template_crear_zumbo";
+	}
+	
+	@PostMapping("/crear")
+	public String mostrarPantallaCrearZumbo(Model model, @RequestParam String input_nombre, @RequestParam Integer input_bibis) {
+		Zumbo nuevo_zumbo = new Zumbo(input_nombre, input_bibis);
+		repositorio_zumbos.save(nuevo_zumbo);
+		
+		return "redirect:/zumbos/lista";
 	}
 
 }
