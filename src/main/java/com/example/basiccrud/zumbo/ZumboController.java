6c95660613d9079499c65196f1edddc8d417b22b
sergioj97 <sergiojsancam@gmail.com>
2022-02-07 16:01:37 +0100

    Añadido @RequestMapping a ZumboController

--- src/main/java/com/example/basiccrud/zumbo/ZumboController.java
+++ src/main/java/com/example/basiccrud/zumbo/ZumboController.java
@@ -4,21 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/zumbos")
 public class ZumboController {
 	
-	@GetMapping("/zumbos/lista")
+	@GetMapping("/lista")
 	public String mostrarPantallaListaZumbos(Model model) {
 		return "template_lista_zumbos";
 	}
 	
-	@GetMapping("/zumbos/detalles/{id}")
+	@GetMapping("/detalles/{id}")
 	public String mostrarPantallaDetallesZumbo(Model model, @PathVariable long id) {
 		return "template_lista_zumbos";
 	}
 	
-	@GetMapping("/zumbos/crear")
+	@GetMapping("/crear")
 	public String mostrarPantallaCrearZumbo(Model model) {
 		return "template_crear_zumbo";
 	}
